5401170b812a77875dd650214dd46b8c7cc86818
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-04-05 10:03:42 +0100

    Thu Apr 05 10:03:42 BST 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -5,33 +5,26 @@
 
 VALID_VALUES = ("A", "B", "C", "D")
 
+
 def checkout(skus):
     for s in skus:
         if s not in VALID_VALUES:
             return -1
-        
 
     total = 0
     a_count = skus.count("A")
-    if a_count:
-        if a_count == 3:  # todo > 3
-            total += 130
-        else:
-            total += (a_count * 50)
+    extra = a_count % 3
+    total += 130 * ((a_count - extra) / 3)
+    if extra:
+        total += extra * 50
 
     b_count = skus.count("B")
-    if b_count:
-        if b_count == 2:  # todo > 3
-            total += 45
-        else:
-            total += b_count * 30
+    extra = b_count % 2
+    total += 45 * ((a_count - extra) / 2)
+    if extra:
+        total += extra * 30
 
     total += skus.count("C") * 20
     total += skus.count("D") * 15
 
     return total
-
-
-print(checkout("AAAA")) # 180
-#print(checkout("AAAAA")) # 230
-#print(checkout("AAAAAA")) # 260
