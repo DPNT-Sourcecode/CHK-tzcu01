352c071ad089fd2bc9e0bc9b53a78ac525bc3403
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-04-05 10:06:08 +0100

    Thu Apr 05 10:06:08 BST 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -13,18 +13,22 @@ def checkout(skus):
 
     total = 0
     a_count = skus.count("A")
-    extra = a_count % 3
-    total += 130 * ((a_count - extra) / 3)
-    if extra:
-        total += extra * 50
+    extra = a_count % 3  # 2
+    if a_count != extra:
+        total += 130 * ((a_count - extra) / 3)
+        if extra:
+            total += extra * 50
 
     b_count = skus.count("B")
     extra = b_count % 2
-    total += 45 * ((a_count - extra) / 2)
-    if extra:
-        total += extra * 30
+    if b_count != extra:
+        total += 45 * ((b_count - extra) / 2)
+        if extra:
+            total += extra * 30
 
     total += skus.count("C") * 20
     total += skus.count("D") * 15
 
     return total
+
+print(checkout("B"))
