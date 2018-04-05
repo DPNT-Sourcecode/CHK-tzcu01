c824b1a04e9a2dc098038ba5f7fcc81a9cee7666
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-04-05 09:57:00 +0100

    Thu Apr 05 09:57:00 BST 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -2,7 +2,27 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+VALID_VALUES = ("A", "B", "C", "D")
+
 def checkout(skus):
+    for s in skus:
+        if s not in VALID_VALUES:
+            return -1
+
+    total = 0
+    a_count = skus.count("A")
+    if a_count:
+        if a_count == 3:  # todo > 3
+            return 130
+        return a_count * 50
+
+    b_count = skus.count("B")
+    if a_count:
+        if a_count == 2:  # todo > 3
+            return 45
+        return a_count * 30
+
     if skus == "A":
         return 50
     if skus == "B":
