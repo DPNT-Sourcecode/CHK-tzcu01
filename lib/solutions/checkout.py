478831083cceb76f3eea8ed926b9b26eb08db95f
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-04-05 11:47:05 +0100

    Thu Apr 05 11:47:05 BST 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -40,12 +40,12 @@ def checkout(skus):
 
     a_count = skus.count("A")  # 3A for 130, 5A for 200
     extra = a_count % 5
-    if a_count != extra:  # at least one special price 200
+    if a_count != extra:  # at least one special price
         total += 200 * ((a_count - extra) / 5)
         a_count -= a_count - extra
     if extra:
         extra = a_count % 3
-        if a_count != extra:  # at least one special price 130
+        if a_count != extra:  # at least one special price
             total += 130 * ((a_count - extra) / 3)
         if extra:
             total += extra * 50
@@ -54,7 +54,7 @@ def checkout(skus):
     e_count = skus.count("E")
     b_count = skus.count("B")
     extra = e_count % 2
-    if e_count != extra and b_count:  # at least one special price 30
+    if e_count != extra and b_count:  # at least one special price
         b_free_count = (e_count - extra) / 2
         replace_times = 0
         while b_count and b_free_count:  # free B
@@ -66,7 +66,7 @@ def checkout(skus):
 
     b_count = skus.count("B")
     extra = b_count % 2  # 2B for 45
-    if b_count != extra:  # at least one special price 45
+    if b_count != extra:  # at least one special price
         total += 45 * ((b_count - extra) / 2)
     if extra:
         total += extra * 30
@@ -85,19 +85,19 @@ def checkout(skus):
 
     h_count = skus.count("H")  # 5H for 45, 10H for 80
     extra = h_count % 10
-    if h_count != extra:  # at least one special price 80
+    if h_count != extra:  # at least one special price
         total += 80 * ((h_count - extra) / 10)
         h_count -= h_count - extra
     if extra:
         extra = h_count % 5
-        if h_count != extra:  # at least one special price 45
+        if h_count != extra:  # at least one special price
             total += 45 * ((h_count - extra) / 5)
         if extra:
             total += extra * 10
 
     k_count = skus.count("K")  # 2K for 150
     extra = k_count % 2
-    if k_count != extra:  # at least one special price 150
+    if k_count != extra:  # at least one special price
         total += 150 * ((k_count - extra) / 2)
     if extra:
         total += extra * 80
@@ -105,7 +105,7 @@ def checkout(skus):
     n_count = skus.count("N")  # 3N get one M free
     m_count = skus.count("M")
     extra = n_count % 3
-    if n_count != extra and m_count:  # at least one special price 30
+    if n_count != extra and m_count:  # at least one special price
         b_free_count = (n_count - extra) / 3
         replace_times = 0
         while m_count and b_free_count:  # free M
@@ -135,7 +135,7 @@ def checkout(skus):
             q_count -= 1
             q_free_count -= 1
         skus = skus.replace("Q", "", replace_times)  # delete free Q
-    total += r_count * 40
+    total += r_count * 50
 
     q_count = skus.count("Q")
     extra = q_count % 3  # 3Q for 80
@@ -144,9 +144,19 @@ def checkout(skus):
     if extra:
         total += extra * 30
 
+    u_count = skus.count("U")  # 3U get one U free
+    extra = u_count % 3
+    if extra != u_count and (u_count > 3 or extra):  # at least one F free
+        if extra:
+            f_free_count = ((u_count - extra) / 3)
+        else:
+            f_free_count = ((u_count - extra) / 3) - 1
+        while u_count and f_free_count:  # free F
+            u_count -= 1
+            f_free_count -= 1
+    total += u_count * 40
 
 
-    # | U    | 40    | 3U get one U free      |
     # | V    | 50    | 2V for 90, 3V for 130  |
 
 
@@ -156,5 +166,5 @@ def checkout(skus):
 
     return total
 
-print(checkout("Q"* 3))
-print(checkout("Q"* 4))
+print(checkout("UUU"))
+print(checkout("UUUUU"))
